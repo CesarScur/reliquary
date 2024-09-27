e597802c14460c3e3f5280e9b5604939be1812e7
Cesar Scur <csrscr@gmail.com>
2024-09-27 00:10:51 -0300

    Add AssetMapper, Turbo and Bootstrap

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
